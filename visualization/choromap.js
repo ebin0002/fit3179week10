ae467e7d908dfcc4d38fe21d1eb865ba8f4ab301
Eusuf Bin Shah Idil <ebin0002@student.monash.edu>
2024-10-06 20:09:27 +1100

    Bunch of nonsense

---
+++ visualization/choromap.js
@@ -0,0 +1,8 @@
+var vg_1 = "map.json";
+var vg_2 = "bar.json";
+vegaEmbed("#map", vg_1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#bar", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
